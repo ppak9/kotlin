102955b1bc48008950d0d7bae8b5ac6c2b87d739
박종현 <jbak@bagjonghyeon-ui-MacBookAir.local>
2024-04-14 17:57:02 +0900

    adding

--- main.kt
+++ main.kt
@@ -2,7 +2,13 @@ fun max(a: Int, b: Int) : Int{
     return if(a > b) a else b
 }
 
+fun greet(name: String){
+    println("hello $name!")
+}
+
 fun main(){
     val c = max(3,2)
     println(c)
+
+    greet("박종현")
 }
