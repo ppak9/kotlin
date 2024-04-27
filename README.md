54eacb847a78b645a090c6c7f64774cf6a94a42b
ppak9 <smartbell1994@gmail.com>
2024-04-27 21:56:47 +0900

    april_27

---
+++ README.md
@@ -0,0 +1,4 @@
+#####Kotlin study
+
+* Basic만이라도 끝내보기
+* [link](https://kotlinlang.org/docs/kotlin-reference.pdf)
--- main.kt
+++ main.kt
@@ -11,6 +11,50 @@ fun checking(){
     }
     println(b)
 }
+fun check_type(){
+  val b : Any = 2
+  if(b is Int){
+    println("is Int")
+  }
+  else{
+    println("None")
+  }
+}
+
+fun list(){
+    var testing : MutableList<String> = mutableListOf("park", "jong")
+    var testingLocked : List<String> = testing // when the list want to locked, correspond origin and new types
+    println(testing[1])
+    println(testingLocked)
+    println(testing.count()) 
+    testing.add("hyun")
+    println(testing.last())
+}
+
+fun map(){
+    var testing : MutableMap<Int,String> = mutableMapOf(100 to "apple")
+    // when you apply the type of the Object(mutable), you must declare the type
+    println(testing.keys)
+}
+
+fun mapTesting(){
+    val num2Word : MutableMap<Int,String> = mutableMapOf(1 to "One", 2 to "Two")
+    val n = 2
+    println("$n is the spelt as '${num2Word[n]}'")
+}
+
+fun condition(){
+    var temp : Int = 25
+    var result : String = when{
+            temp < 10 -> "cold"
+            temp < 20 -> "warm"
+            temp > 20 -> "hotter" 
+            else -> "None"
+    }
+    println(result)
+}
+    
+
 fun main(){
-    checking()
+    condition()
 }
